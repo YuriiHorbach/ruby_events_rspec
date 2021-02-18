99242e37f003a7f961de30dafc1eea076fa055f5
user <gyrii84@gmail.com>
2021-02-18 21:39:19 +0200

    add some refactoring to index page and spec file

--- spec/features/list_events_spec.rb
+++ spec/features/list_events_spec.rb
@@ -4,7 +4,8 @@ require 'rails_helper'
 
 describe "Viewing the list of the events" do
     it "show the event" do
-        visit "html://example.com/events"
+        visit events_url
+
 
         expect(page).to have_text('3 Events')
         expect(page).to have_text('Event 1')
