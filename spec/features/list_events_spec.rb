caf3522b88e94419ca5de59c3fd83524344284ab
YuriiHorbach <gyrii84@gmail.com>
2021-02-18 14:16:47 +0200

    install rsepc and capybara and create first spec

---
+++ spec/features/list_events_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'rails_helper'
+
+
+describe "Viewing the list of the events" do
+    it "show the event" do
+        visit "html://example.com/events"
+
+        expect(page).to have_text('3 Events')
+        expect(page).to have_text('Event 1')
+        expect(page).to have_text('Event 2')
+        expect(page).to have_text('Event 3')
+    end
+
+   
+
+
+
+end
